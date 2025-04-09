0468f64c521cb41426d60f0599f957fd2754a160
deenoloo <doreenoloo2@gmail.com>
2025-04-09 01:22:19 -0400

    first changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio, links}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.trim() !== "" ? (
+        <p>{bio}</p>
+      ) : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={links.github} linkedin={links.linkedin}/>
       {/* add your <Links /> component here */}
     </div>
   );
